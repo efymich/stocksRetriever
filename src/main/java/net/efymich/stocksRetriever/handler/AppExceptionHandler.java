bbbfa2c5253eb5c6adc1fc1f0da2cf9dc6de69a7
efymich <nn.efymovich@gmail.com>
2024-05-17 16:12:07 +0300

    refactoring

--- src/main/java/net/efymich/stocksRetriever/handler/AppExceptionHandler.java
+++ src/main/java/net/efymich/stocksRetriever/handler/AppExceptionHandler.java
@@ -18,20 +18,6 @@ public class AppExceptionHandler {
         return ResponseEntity.status(NOT_FOUND).body(new ApiErrorResponse(NOT_FOUND.value(), e.getMessage()));
     }
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<ApiErrorResponse> handleRequestNotValidException(MethodArgumentNotValidException e) {
-//
-//        List<String> errors = new ArrayList<>();
-//        e.getBindingResult()
-//                .getFieldErrors().forEach(error -> errors.add(error.getField() + ": " + error.getDefaultMessage()));
-//        e.getBindingResult()
-//                .getGlobalErrors() //Global errors are not associated with a specific field but are related to the entire object being validated.
-//                .forEach(error -> errors.add(error.getObjectName() + ": " + error.getDefaultMessage()));
-//
-//        String message = "Validation of request failed: %s".formatted(String.join(", ", errors));
-//        return ResponseEntity.status(BAD_REQUEST).body(new ApiErrorResponse(BAD_REQUEST.value(), message));
-//    }
-
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ApiErrorResponse> handleValidationException(ValidationException e) {
         return ResponseEntity.status(BAD_REQUEST).body(new ApiErrorResponse(BAD_REQUEST.value(), e.getMessage()));
@@ -44,6 +30,7 @@ public class AppExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse> handleUnknownException(Exception e) {
-        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiErrorResponse(INTERNAL_SERVER_ERROR.value(), e.getMessage()));
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                .body(new ApiErrorResponse(INTERNAL_SERVER_ERROR.value(), e.getMessage()));
     }
 }
